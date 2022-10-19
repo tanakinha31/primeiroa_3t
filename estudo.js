bca72a6dbcb45892a0e20dab675828eeb1171d76
tanakinha31 <111132281+tanakinha31@users.noreply.github.com>
2022-10-19 08:14:12 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,63 @@
+let tabuada = 13;
+let nome = "Carol"
+function escreva () {
+  document.write("Tabuada do " + tabuada + "<br>")
+    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
+document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
+document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
+document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
+document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
+document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
+document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
+document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
+document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
+document.write(tabuada + " x 10 = "+(tabuada*10)+"<br>");
+document.write("Feito por " + nome);
+}
 
+function minhaTabuada(){
+  for(let i = 1; i <= 10; i++){
+    document.write("Tabuada do " + i + "<br>");
+    for(let j = 1; j <= 10; j++){
+      document.write(i + " x " + j + " = "+(j*i)+"<br>");
+    }
+    document.write("<br>");
+    
+  }
+}
+
+function quadrado (){
+  for(let i = 2; i <= 20; i++){
+    document.write("O quadrado de " + 1 + " é " + (i*i) + "<br>");
+  }
+}
+
+
+function calcula() {
+  let c = document.getElementById("valor").value;
+  let j = document.getElementById("juros").value;
+  let r = c * (1 + (j/100));
+  document.write("Resultado: " + r);
+}
+
+
+
+document.write(Resultado: R$ + r)
+}
+
+let operacao = "";
+fuction operacao (op)
+
+}
+fuction calcule()}
+Let v1 = document.getElementById("V1").value
+ler v1 = document.getElementById("v1").value
+let r = 0;
+̣̣̣̣̣̣̣̣̣// isso deve ser duplicado a cada botão
+if(op == "+"){  
+r = Number(v1) + Number(v2);
+}
+
+document.getElementById("resultado").innerHTML = r;
+
+}
